44aba745fa14fc1474c408aa9b8ff64814338f1d
abstraced <antoine.luc@gmail.com>
2019-06-19 10:48:48 +0200

    function biggerOne added

--- js/scripts.js
+++ js/scripts.js
@@ -16,15 +16,31 @@ var repository= [
   types:['grass','rock']
   }
 ]
-var test = document.write('this is a test');
 
 
+function theBiggerOne () {
+  var theHighest= 0;
+  var pos;
+   for ( var j=0; j<repository.length; j++) {
 
+       if (theHighest <repository[j].height) {
+        theHighest= repository[j].height;
+        pos=j;
+       }
+      repository[j].height
+  }
+  return pos;
+}
+
+var pos = theBiggerOne();
 
 for (var i=0; i<repository.length; i++){
 
-  document.write('<br>' + repository[i].name + ' - Height: '+ repository[i].height+ 'm' +  '<br>');
+  document.write('<br>' + repository[i].name + ' - Height: '+ repository[i].height+ 'm');
+  if ( i=== pos){
+  document.write(' That\'s the bigger one!');
 
+  document.write('<br>' + '<br>')
+  }
 
-
-};
+}
