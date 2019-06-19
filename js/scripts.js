4a82c8839e7c0ecfe9ab28b4ac388dc28f464c05
abstraced <antoine.luc@gmail.com>
2019-06-19 10:39:10 +0200

    display list Pokemon document.write()

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
-alert('Hello world');
+var repository= [
+  {name:"Bulbasaur",
+  height:0.7,
+  types:['grass', 'poison'],
+  },
+  {name:"Pikachu",
+  height:0.4,
+  types:['electric']
+  },
+  {name:"Machop",
+  height:0.8,
+  types:['fighting']
+  },
+  {name:"Lileep",
+  height:1,
+  types:['grass','rock']
+  }
+]
+var test = document.write('this is a test');
+
+
+
+
+for (var i=0; i<repository.length; i++){
+
+  document.write('<br>' + repository[i].name + ' - Height: '+ repository[i].height+ 'm' +  '<br>');
+
+
+
+};
